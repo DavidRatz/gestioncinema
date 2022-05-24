d0daef13e8d99265d6121c7c03cf8733ea4f790f
DavidRatz <david.ratz@hotmail.com>
2022-05-24 16:53:39 +0200

    gestioncinema - init + config

---
+++ programmation/src/main/java/be/technifutur/programmation/ProgrammationApplication.java
@@ -0,0 +1,13 @@
+package be.technifutur.programmation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgrammationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgrammationApplication.class, args);
+	}
+
+}
---
+++ programmation/src/test/java/be/technifutur/programmation/ProgrammationApplicationTests.java
@@ -0,0 +1,13 @@
+package be.technifutur.programmation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgrammationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
